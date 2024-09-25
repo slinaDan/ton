48273525891b956ab8bb5c845f7f4b5033176c82
jindandan <jindd826@163.com>
2024-09-25 09:26:00 +0800

    测试

---
+++ assets/one-CXrO1K9a.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../imgs/one.gif",import.meta.url).href;export{e as _};
---
+++ assets/use-placeholder-C8hS1JEN.js
@@ -0,0 +1,1 @@
+import{r as h,o as n,U as i,N as r,w as u,V as c,W as l,av as d,f as g}from"./index-Cv6Vz8a5.js";const p=(o,a)=>{const t=h(),e=()=>{t.value=d(o).height};return n(()=>{i(e);for(let s=1;s<=3;s++)setTimeout(e,100*s)}),r(()=>i(e)),u([c,l],e),t};function f(o,a){const t=p(o);return e=>g("div",{class:a("placeholder"),style:{height:t.value?`${t.value}px`:void 0}},[e()])}export{f as u};
